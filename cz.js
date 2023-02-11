cf63242358498bd50f55dae682a22cd1d88b7335
nsmclt <123085231+nsmclt@users.noreply.github.com>
2023-02-11 21:48:04 +0800

    Create cz.js

---
+++ cz.js
@@ -0,0 +1,33 @@
+  document.getElementById("ik").innerHTML=`
+
+    <div class="c-1">
+
+      <li class="m-1">
+
+         <br>
+
+         <h3>最新消息</h3>
+
+         <br>
+
+         <a>“Welcome”</a></p>
+
+         <br>
+
+         <a href="gy.html">“更新内容”</a></p>
+
+         <br>
+
+         <a href="c.html">“C版声明”</a></p>
+
+         <br>
+
+         <br>
+
+         <br>
+
+      </li class="m-1">
+
+    </div class="c-1">
+
+  </div>`;
