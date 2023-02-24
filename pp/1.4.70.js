36a6ec4261bb4a3acce62b59ac2d3934b8d3b4aa
nsmclt <123085231+nsmclt@users.noreply.github.com>
2023-02-24 21:10:45 +0800

    Update 1.4.70.js

--- pp/1.4.70.js
+++ pp/1.4.70.js
@@ -1,1 +1,50 @@
 mui.alert("最新版已发布请前往网站更新</p>https://nsmclt.github.io/", "更新提示");
+   document.write(`
+
+<div id="popu1" style="position: fixed; top: 0; bottom: 0; left: 0; right: 0; background-color: rgba(0, 0, 0, 0.5); z-index: 999;">
+
+    <div style="position: absolute; top: 50%; left: 50%; transform: translate(-50%, -65%);width: 70vw; margin-left:calc(50% - 50vw);margin-right: calc(50% - 50vw);margin: 0 auto; height: 205px; background-color: #ffffff; padding: 10px;border-radius:12px;">
+       <p style="text-align:center;transform:translate(0px,-6px);">提示</p>
+       <div style="width: 63vw; margin-left:calc(50% - 50vw);margin-right: calc(50% - 50vw);margin: 0 auto;font-size:12px;transform:translate(0px,-5px);">
+       感谢您使用GJ-C,以下为用户交流群(点击复制)
+       <p onclick="copyText('724469660')" style="size-font:12px">一群:724469660</p>
+       <p onclick="copyText('737977468')" style="size-font:12px;transform:translate(0px,-10px);">二群:737977468</p>
+       
+       <div style="width: 63vw; margin-left:calc(50% - 50vw);margin-right: calc(50% - 50vw);margin: 0 auto;font-size:12px;transform:translate(0px,-5px);text-align:center;">
+         <button onclick="document.getElementById('popu1').style.display='none'" style="box-shadow:0 1px 2px rgba(0,0,0,0.15);border:0px solid;color:#fff;border-radius:30px;width:100%;height:30px;font-size:11px;background-color:#1E9fff;">确定</button>
+         <button onclick="document.getElementById('popu1').style.display='none'" style="border:0px solid;color:grey;border-radius:30px;width:100%;height:30px;font-size:11px;background-color:#fff;">取消</button>
+       </div>
+    </div>
+    </div> `);
+    document.write(`
+
+<div id="popu" style="position: fixed; top: 0; bottom: 0; left: 0; right: 0; z-index: 999;">
+
+    <div style="position: absolute; top: 53%; left: 50%; transform: translate(-50%, -65%);width: 70vw; margin-left:calc(50% - 50vw);margin-right: calc(50% - 50vw);margin: 0 auto; background-color: #ffffff; padding: 10px;border-radius:12px;">
+
+       <div style="text-align:center;width: 63vw; margin-left:calc(50% - 50vw);margin-right: calc(50% - 50vw);margin: 0 auto;font-size:12px;transform:translate(0px,-20px);">
+        <div id="gx" style="color:grey;">
+        
+        <br><br>
+     
+     <iframe frameborder="0" style="transform:translate(0px,74px);width:230px;" src="https://nsmclt.github.io/gxnr.html"></iframe>
+     
+     
+        </div>
+       </div>
+       <div style="width: 63vw; margin-left:calc(50% - 50vw);margin-right: calc(50% - 50vw);margin: 0 auto;font-size:12px;transform:translate(0px,-5px);text-align:center;">       <div style="width: 63vw; margin-left:calc(50% - 50vw);margin-right: calc(50% - 50vw);margin: 0 auto;font-size:12px;transform:translate(0px,-5px);text-align:center;">
+        <form action="https://nsmclt.github.io/np/np.apk">
+         <button onclick="document.getElementById('popu').style.display='none'" style="box-shadow:0 1px 2px rgba(0,0,0,0.15);border:0px solid;color:#fff;border-radius:30px;width:100%;height:30px;font-size:11px;background-color:#1E9fff;">更新</button>
+        </form>
+       </div>
+    <div style="position: absolute;transform: translate(-50%, -65%); top: -455%; left: 50%;background-color:#2962FF;width:75vw;height:110px; margin-left:calc(50% - 50vw);margin-right: calc(50% - 50vw);margin: 0 auto;border-top-left-radius: 12px;border-top-right-radius: 12px;">
+         <p style="transform:translate(0px,-10px);font-size:21px;text-align:center;color:#fff;">检测到新版本</p>
+       <div style="height:27px;width: 63vw; margin-left:calc(50% - 50vw);margin-right: calc(50% - 50vw);margin: 0 auto;text-align:center;transform:translate(0px,-15px);background-color:#529fff;border-radius:8px;color:#fff;">
+       <p style="font-size:11px;transform:translate(-45px,5px);">当前版本:1.4.70</p>
+       <p id="zx" style="font-size:11px;transform:translate(45px,-21.5px);">最新版本:x.x.x</p>
+       </div>
+    </div>
+    </div>
+    </div>
+    <button onclick="document.getElementById('popu').style.display='none'" style="height:40px;border:1px solid;color:#fff;border-radius:50px;position: absolute; top: 53%; left: 50%; transform: translate(-50%, 300%);font-size:15px;background-color:transparent;width:40px;">✕</button>
+  `)
