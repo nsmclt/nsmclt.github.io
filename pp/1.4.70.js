1a7a5c03ec03e062f1b8145a4b2c4ff0d080220d
nsmclt <123085231+nsmclt@users.noreply.github.com>
2023-03-26 11:18:02 +0800

    Update 1.4.70.js

--- pp/1.4.70.js
+++ pp/1.4.70.js
@@ -1,1 +1,1 @@
-mui.alert("检测到最新版本 1.3.43</p><a href='https://nsmclt.github.io/'>https://nsmclt.github.io/网址下载</a>", "提示");
+mui.alert("检测到最新版本</p><a href='https://nsmclt.github.io/'>https://nsmclt.github.io/网址下载</a>", "提示");
