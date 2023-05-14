488abac813a4d164429c8b8a2eefc7ccd1a2fa55
nsmclt <123085231+nsmclt@users.noreply.github.com>
2023-05-14 16:59:04 +0800

    Update gfxx.js

--- nslt/gfxx.js
+++ nslt/gfxx.js
@@ -1,33 +1,57 @@
-
     function showPage(pageName) {
+
       var pages = document.querySelectorAll('.page-container');
+
       
+
       // 隐藏所有页面
+
       for (var i = 0; i < pages.length; i++) {
+
         pages[i].classList.remove('page-active');
+
       }
 
       // 显示目标页面
+
       var targetPage = document.querySelector('.' + pageName);
+
       targetPage.classList.add('page-active');
+
       document.getElementById("gxx").innerHTML = " ";
+
     }
+
   function showPa() {
+
       document.getElementById("gxx").innerHTML = xx;
+
       document.getElementById("p2").innerHTML = " ";
+
       
+
       
+
   }
 
 var p2 = `
+
    <div style="width:100%;height:0.6px;background-color:#eee;"><div>
+
    <br>
+
   <div class="container">
+
    <div class="tx">
+
    </div>
+
    <p style="transform:translate(13px,0px);font-size:13px;font-weight: bold;color:#333;">托更永远至上 </p>
+
    <p style="transform:translate(15px,0px);font-size:13px;font-weight: ;color:#666;"> @GJ-C · 下载</p>
+
    </div>
+
    
 
    <div class="nr">
@@ -47,51 +71,116 @@ var p2 = `
    <p style="background-color:red;width:126px;font-weight: bold;color:#fff;">仅供体验，支持正版</p>
 
    </div>
+
    <div style="width:100%;height:0.6px;background-color:#eee;"><div>
 
    <div style="width:100%;height:0.6px;background-color:#eee;"><div>
+
    <br>
+
   <div class="container">
+
    <div class="tx">
+
    </div>
+
    <p style="transform:translate(13px,0px);font-size:13px;font-weight: bold;color:#333;">托更永远至上 </p>
+
    <p style="transform:translate(15px,0px);font-size:13px;font-weight: ;color:#666;"> @GJ-C · 下载</p>
+
    </div>
+
    <div class="nr">
+
    <a href="https://nsmclt.github.io/official.html?" style="font-weight: bold;color:#1e9fff">作者软件，感兴趣的可以下载一下~</a>
+
    </div>
+
    
+
    <div style="width:100%;height:0.6px;background-color:#eee;"><div>
+
    <br>
+
   <div class="container">
+
    <div class="tx">
+
    </div>
+
    <p style="transform:translate(13px,0px);font-size:13px;font-weight: bold;color:#333;">托更永远至上 </p>
+
    <p style="transform:translate(15px,0px);font-size:13px;font-weight: ;color:#666;"> @GJ-C · 娱乐</p>
+
    </div>
+
    <div class="nr">
+
    <a href="yx.php" style="font-weight: bold;color:#1e9fff">小姐姐全在这条链接内哦~</a>
+
    </div>
+
    
+
    <div style="width:100%;height:0.6px;background-color:#eee;"><div>
+
 `;
+
  var xx = `
+
  
+
    <div style="width:100%;height:0.6px;background-color:#eee;"><div>
+
    <br>
+
   <div class="container">
+
    <div class="tx">
+
    </div>
+
    <p style="transform:translate(13px,0px);font-size:13px;font-weight: bold;color:#333;">托更永远至上 </p>
-   <p style="transform:translate(15px,0px);font-size:13px;font-weight: ;color:#666;"> @GJ-C · 2023年5月13日</p>
+
+   <p style="transform:translate(15px,0px);font-size:13px;font-weight: ;color:#666;"> @GJ-C · 2023年5月14日 · 母亲节</p>
+
    </div>
+
    <div class="nr">
-   第一条帖子
+
+   母亲节快乐！祝天下的妈妈快乐！
+
    </div>
+
+   <div style="width:100%;height:0.6px;background-color:#eee;"><div>
+
+   <br>
+
+  <div class="container">
+
+   <div class="tx">
+
+   </div>
+
+   <p style="transform:translate(13px,0px);font-size:13px;font-weight: bold;color:#333;">托更永远至上 </p>
+
+   <p style="transform:translate(15px,0px);font-size:13px;font-weight: ;color:#666;"> @GJ-C · 2023年5月13日</p>
+
+   </div>
+
+   <div class="nr">
+
+   第一条帖子
+
+   </div>
+
    
+
    <div style="width:100%;height:0.5px;background-color:#eee;"><div>
+
 `;
 
 document.getElementById("gxx").innerHTML = xx;
+
 document.getElementById("p2").innerHTML = p2;
 
