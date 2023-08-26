007f2273402e2ebcc98baae50d20091a68e6a9e2
nsmclt <123085231+nsmclt@users.noreply.github.com>
2023-08-26 17:00:47 +0800

    Add files via upload

---
+++ sanku/gonggao.js
@@ -0,0 +1,41 @@
+
+   var gonggao = `
+     <br>
+    
+    <p style="transform:translate(0px,8px);">公告</p>
+    <p style="font-size:15px;">SANKU是GJ-C的子版本</p>
+    <p style="font-size:15px;">看什么看</p>
+   `;
+   document.getElementById("gonggao").innerHTML = gonggao;
+   
+   
+   var yx = `
+   
+    <a href="https://s.readlo.com/?id=2c9fe67689b87c0a0189f7e42fe20449">
+     <div class="yxapp">
+       <p style="font-size:10px;transform:translate(0px,30px);display: flex;align-items: center;  justify-content: center; ">SK冒险记</p>
+       <p style="font-size:13px;transform:translate(0px,65px);color:#777;text-align:center;">作者:SANKU</p>
+      </div> 
+      <p style="font-size:9px;transform:translate(0px,20px);color:#888;text-align:center;width:100%;">目前作者正在上学，这个小游戏是作者用scratch抽空制作的，当然软件也一样</p>
+   </a>
+    
+    <br>
+    <a href="https://s.readlo.com/?id=2c9fe676729e8e3001729e97b3850003">
+     <div class="yxapp">
+       <p style="font-size:10px;transform:translate(0px,30px);display: flex;align-items: center;  justify-content: center; ">植物大战僵尸3.0</p>
+       <p style="font-size:13px;transform:translate(0px,65px);color:#777;text-align:center;">作者:刘嘉*</p>
+      </div> 
+      <p style="font-size:9px;transform:translate(0px,20px);color:#888;text-align:center;width:100%;">使用scratch制作的植物大战僵尸，与原版根本没有区别！</p>
+   </a>
+    
+    <br>
+    <a href="https://s.readlo.com/?id=2c9fe67673c13be30173ec83ba5617d2">
+     <div class="yxapp">
+       <p style="font-size:10px;transform:translate(0px,30px);display: flex;align-items: center;  justify-content: center; ">2D我的世界</p>
+       <p style="font-size:13px;transform:translate(0px,65px);color:#777;text-align:center;">作者:未知</p>
+      </div> 
+      <p style="font-size:9px;transform:translate(0px,20px);color:#888;text-align:center;width:100%;">玩够了3D我的世界(minecraft)玩玩2D的吧！</p>
+   </a>
+   `;
+   document.getElementById("yx").innerHTML = yx;
+   
